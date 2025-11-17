798e3ccd635bebff743818ef381db250b69074f4
Grigory Rechistov <grigory.rechistov@gmail.com>
2025-11-17 22:23:17 +0100

    Add warning comment

--- cyclomatic/calculator/__init__.py
+++ cyclomatic/calculator/__init__.py
@@ -1,2 +1,3 @@
 from cyclomatic.calculator.calculator import calculate
+# Importing these classes below causes side effects on LANGUAGE_MAPPING
 from cyclomatic.calculator.py_calculator import PyCalculator
