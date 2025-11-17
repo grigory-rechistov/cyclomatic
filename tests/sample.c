8a50f084c3503b65d814fbb5ae3c3ce48c6752fc
Grigory Rechistov <grigory.rechistov@gmail.com>
2025-11-17 21:53:36 +0100

    Add sample C file

---
+++ tests/sample.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+// Main function: entry point for execution
+int main() {
+    int a = 0;
+    int b = 1;
+
+    printf("Hello World");
+
+    if (a == b) {
+        return 1;
+    }
+    return 0;
+}
