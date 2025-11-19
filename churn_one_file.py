9aef3fb1ea62b33033ab279e95363df5f41a8303
Grigory Rechistov <grigory.rechistov@gmail.com>
2025-11-19 07:09:09 +0100

    Make get churn exportable
    Additionally, run git in the same working directory as file, to allow it
    to find .git folder.

--- churn_one_file.py
+++ churn_one_file.py
@@ -1,25 +1,30 @@
 # Calculate and print churn for given file path
 import subprocess
+from pathlib import Path
 import sys
 
 
-def main(path):
+def get_churn(path: str) -> int:
     # Query git for one line per commit touching the file, count number of
     # returned lines
-    cmd = ("git", "log", "--oneline", "--", path)
-    p = subprocess.Popen(cmd, stdout=subprocess.PIPE)
+    p = Path(path)
+    cwd = str(p.parent)
+    file = str(p.name)
+    cmd = ("git", "log", "--oneline", "--", file)
+    p = subprocess.Popen(cmd, stdout=subprocess.PIPE, cwd=cwd)
     churn = sum(1 for _ in p.stdout)
     p.wait()
     status = p.poll()
     p.stdout.close()
     if status != 0:
-        print(f"git failed with error code {status}", file=sys.stderr)
-        return 1
+        raise RuntimeError(f"git failed with error code {status}")
+    return churn
 
-    print(churn)
+
+def main(path):
+    print(get_churn(path))
     return 0
 
 
 if __name__ == "__main__":
     sys.exit(main(sys.argv[1]))
-
