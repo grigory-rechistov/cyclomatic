7350a2aef6fe5357ced31870cbe3909fff87c9a9
Grigory Rechistov <grigory.rechistov@gmail.com>
2025-11-21 09:09:46 +0100

    Increase Python stack limit
    The default is 1000, which is not enough to analyse some of files
    in my corpus. This caused RecursionError while visiting a long
    expression.

--- cyclomatic/__init__.py
+++ cyclomatic/__init__.py
@@ -4,3 +4,8 @@ from cyclomatic.cyclomatic import (
     cyclomatic_in_batch,
     cyclomatic_in_parallel
 )
+
+# Analyzed code may have crazy high depth.
+# Increase the limit if analysis fails with RecursionError
+import sys
+sys.setrecursionlimit(10000)
