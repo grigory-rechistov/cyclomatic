5658eb33de9fff09b50e1eaee152520ec1c5aff5
Grigory Rechistov <grigory.rechistov@gmail.com>
2025-11-24 07:36:55 +0100

    Use better grammar

--- cyclomatic/config.py
+++ cyclomatic/config.py
@@ -1,4 +1,4 @@
-# This mapping is dynamically updated on runtime when classes and modules are
+# This mapping is dynamically updated at runtime when classes and modules are
 # loaded. Very nasty mechanism based on __init_subclass__ hook magic
 LANGUAGE_MAPPING = {
     'py': [None, 'python', None],
